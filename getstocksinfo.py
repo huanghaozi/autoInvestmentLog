8ccc3b98fa3a403ac696a07e5cda1d49816d330e
HH <huang_hao_zi@163.com>
2020-11-26 19:25:27 +0800

    Update getstocksinfo.py

--- getstocksinfo.py
+++ getstocksinfo.py
@@ -1,10 +1,14 @@
 # coding: utf-8
 import akshare as ak
-import pandas as pd
+import os
 import time
+localtime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+if not os.path.exists(localtime):
+    os.mkdir(localtime)
 try:
     df_Stock = ak.stock_zh_a_spot()
-    df_Stock.to_json('A_data.json')
+    df_Stock.to_json(localtime + '/A_data.json')
+
 except:
     pass
 
@@ -16,7 +20,7 @@ try:
     shfe_text = ak.match_main_contract(exchange="shfe")
     df_Future_C = ak.futures_zh_spot(subscribe_list=",".join([dce_text, czce_text, shfe_text])
                                      ,adjust=False,market="CF")
-    df_Future_C.to_json('Future_C_data.json')
+    df_Future_C.to_json(localtime + '/Future_C_data.json')
 except:
     pass
 
@@ -25,7 +29,7 @@ time.sleep(10)
 try:
     cffex_text = ak.match_main_contract(exchange="cffex")
     df_Future_F = ak.futures_zh_spot(subscribe_list=cffex_text, market="FF", adjust=False)
-    df_Future_F.to_json('Future_F_data.json')
+    df_Future_F.to_json(localtime + '/Future_F_data.json')
 except:
     pass
 
@@ -34,6 +38,6 @@ time.sleep(10)
 try:
     subscribe_list = ak.hf_subscribe_exchange_symbol()
     df_Future_W = ak.futures_hf_spot(subscribe_list=subscribe_list)
-    df_Future_W.to_json('Future_W_data.json')
+    df_Future_W.to_json(localtime + '/Future_W_data.json')
 except:
     pass
